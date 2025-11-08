b94cdc0fb3e51e1f17e2431239a6880bd41a48a7
techted89 <45821909+techted89@users.noreply.github.com>
2025-11-08 03:19:34 -0800

    Update build.gradle.kts

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
      id("com.android.application") version "8.1.1"
      id("org.jetbrains.kotlin.android") version "1.8.10"          
-id("org.jetbrains.kotlin.plugin.compose")version "1.8.10"
+id("org.jetbrains.kotlin.plugin.compose") version "1.8.10"
 
 }
 
