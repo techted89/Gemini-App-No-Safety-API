f77916f151ccb2232de34557016934312f864ee4
techted89 <45821909+techted89@users.noreply.github.com>
2025-11-07 16:26:24 -0800

    Update MainActivity.kt

--- App/src/main/java/com/techtedapps/geminichat/MainActivity.kt
+++ App/src/main/java/com/techtedapps/geminichat/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.geminichat.ui.ChatScreen
-import com.example.geminichat.ui.theme.GeminiChatTheme
+import com.techtedapps.geminichat.ui.ChatScreen
+import com.techtedapps.geminichat.ui.theme.GeminiChatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
